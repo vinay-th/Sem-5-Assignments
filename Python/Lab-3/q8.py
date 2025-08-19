c07855dce4545df67864c7aa743c16d87729b55b
Vinay Thakor <vinay.th.24@gmail.com>
2025-08-19 20:06:58 +0530

    Add Python Lab 3 exercise: implement a function to display student details using keyword arguments

---
+++ Python/Lab-3/q8.py
@@ -0,0 +1,8 @@
+# 8. Write a function to display student details (name, age, course) using keyword arguments.
+
+def display_student_details(name, age, course):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Course: {course}")
+
+display_student_details(name="Bhupendra", age=20, course="Computer Science")
