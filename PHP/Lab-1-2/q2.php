e25a56c22d6d9fcac6b22d41eb5de95d3498094b
Vinay Thakor <vinay.th.24@gmail.com>
2025-11-17 10:22:33 +0530

    Refactor PHP program to display current age

--- PHP/Lab-1-2/q2.php
+++ PHP/Lab-1-2/q2.php
@@ -1,12 +1,18 @@
-<!-- 2 Create a php program with an HTML header and a PHP block that displays your currentage using a variable. -->
-<html>
-    <body>
-        <h1>
-            <?php
-                $age = 19;
+<!-- 2 Create a php program with an HTML header and a PHP block that displays your current age using a variable. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Q2</title>
+</head>
+<body>
+    <header>
+    <?php 
+        $age = 20;
 
-                echo "My age is $age";
-            ?>
-        </h1>
-    </body>
+        echo "Your current age is $age";
+    ?>
+    </header>
+</body>
 </html>
