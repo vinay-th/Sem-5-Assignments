5649a0c899b0d62eecbfaaea0ad27d173bb340ca
Vinay Thakor <vinay.th.24@gmail.com>
2025-11-19 13:32:15 +0530

    Refactor word analysis logic in q12.py
    Refactored vowel and consonant counting logic to iterate through each character in words. Added comments and improved code readability.

--- Python/Assignment-2/q12.py
+++ Python/Assignment-2/q12.py
@@ -1,26 +1,26 @@
-# Input a paragraph and count the number of words, vowels, and consonants.
+# 12 Input a paragraph and count the number of words, vowels, and consonants.
 # Store word frequencies in a dictionary.
-
-para = input("Enter a paragraph: ")
+para = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
 words = para.split()
 word_count = len(words)
 
 vowels = 0
 consonants = 0
-word_frequencies = {} # Dictionary to store word frequencies
+word_frequencies = {}
 
 for word in words:
     if word in word_frequencies:
         word_frequencies[word] += 1
     else:
         word_frequencies[word] = 1
-    if word.lower() in 'aeiou':
-        vowels += 1
-    else:
-        consonants += 1
+    for ch in word.lower():
+        if ch in 'aeiou':
+            vowels += 1
+        else:
+            consonants += 1
 
-    # Print the results
+
 print("Word count:", word_count)
 print("Vowel count:", vowels)
 print("Consonant count:", consonants)
